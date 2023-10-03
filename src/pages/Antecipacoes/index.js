2c575118c17af0cc31990c9b63bb92bd54db5669
isabelaguilhon <isabelaguilhon.f@gmail.com>
2023-10-03 15:49:47 -0300

    wip: adicionando layout da pagina de antecipações

--- src/pages/Antecipacoes/index.js
+++ src/pages/Antecipacoes/index.js
@@ -1,74 +1,47 @@
 
 import React, { useEffect, useState } from "react";
 import './antecipacoes.css';
+import Calendar from "react-calendar"
 
-
-  function TabelaFiltrada() {
+  export default function TabelaFiltrada() {
     const [filtro1, setFiltro1] = useState("all");
     const [filtro2, setFiltro2] = useState("all");
     const [filtro3, setFiltro3] = useState("all");
     const [dadosFiltrados, setDadosFiltrados] = useState([]);
-    const [mostrarConteudo1, setMostrarConteudo1] = useState(true);
-    const [mostrarConteudo2, setMostrarConteudo2] = useState(false);
+    const [dataBusca, setDataBusca] = useState(new Date())
+    const [buscou, setBuscou] = useState(false)
   
     const dados = [
       { id: 1, col1: "One", col2: "A", col3: "X" },
       { id: 2, col1: "Two", col2: "B", col3: "Y" },
       { id: 3, col1: "Three", col2: "C", col3: "Z" },
     ];
+
+    function handleDateChange(date){
+      setDataBusca(date)
+
+    }
   
     useEffect(() => {
       const dadosFiltrados = dados.filter((item) => {
         const filtro1Pass = filtro1 === "all" || item.col1 === filtro1;
         const filtro2Pass = filtro2 === "all" || item.col2 === filtro2;
         const filtro3Pass = filtro3 === "all" || item.col3 === filtro3;
-  
         return filtro1Pass && filtro2Pass && filtro3Pass;
       });
   
       setDadosFiltrados(dadosFiltrados);
     }, [filtro1, filtro2, filtro3]);
   return (
+
+  <div className="page-antecipacoes">
     <div className="card">
       <div className="card-header">
-        <div className="btn-group" role="group">
-          <button 
-            type="button"
-            className="btn-conteudo"
-            onClick={() => {
-              setMostrarConteudo1(true);
-              setMostrarConteudo2(false);
-            }}
-          >
-            Solicitar Antecipação
-          </button>
-          <button
-            type="button"
-            className="btn-conteudo"
-            onClick={() => {
-              setMostrarConteudo1(false);
-              setMostrarConteudo2(true);
-            }}
-          >
-            Solicitações Realizadas
-          </button>
+            <h1 className="title-anteipacoes"> Antecipações </h1> 
         </div>
-      </div>
-      {mostrarConteudo2 && (
 
     <div className="card-body">
       <div className="filtro">
-        <select
-          className="form-select"
-          aria-label="Filtro 1"
-          value={filtro1}
-          onChange={(e) => setFiltro1(e.target.value)}
-        >
-          <option value="all">Todos</option>
-          <option value="One">One</option>
-          <option value="Two">Two</option>
-          <option value="Three">Three</option>
-        </select>
         <select
           className="form-select"
           aria-label="Filtro 2"
@@ -91,10 +64,16 @@ import './antecipacoes.css';
           <option value="Y">Y</option>
           <option value="Z">Z</option>
         </select>
+
+        <div className='submit-container'>
+            <button className="btn btn-primary btn-submit">Pesquisar</button>
+          </div>      
       </div>
-      <table className="table table-dark table-striped">
+
+    <div className="table-responsive">
+      <table className="table table-bordered table-dark">
         <thead>
-          <tr>
+          <tr class="table-secundary">
             <th scope="col">#</th>
             <th scope="col">Coluna 1</th>
             <th scope="col">Coluna 2</th>
@@ -111,17 +90,20 @@ import './antecipacoes.css';
             </tr>
           ))}
         </tbody>
-      </table>
+      </table>  
     </div>
+      <Calendar
+         onChange={ handleDateChange }
+         value={ dataBusca }
+         onClick={ console.log(dataBusca) }
+        />
+      </div>    
+    </div>
+  </div>
 
-      )}
-      {mostrarConteudo1 && (
-        <div className="card-body">
-          <p>Este é o conteúdo 2 que aparecerá quando você clicar em "Mostrar Conteúdo 2".</p>
-        </div>
-      )}
-    </div>
   );
 };
- 
-export default TabelaFiltrada;
+
+
+
+
