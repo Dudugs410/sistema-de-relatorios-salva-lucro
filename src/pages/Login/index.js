75006276caf794cde0c80dc86fa6f788613897b6
isabelaguilhon <isabelaguilhon.f@gmail.com>
2023-10-05 15:38:59 -0300

    feat: alterando layout da página de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/auth"
 import Cookies from "js-cookie"
-
-import salvaLucroLogo from '../../assets/salva-lucro-logo.jpg'
-
+import salvaLucroLogo from '../../assets/logoSalvaLucro.png'
 import './login.css'
 import { useContext } from "react"
 import LoadingModal from "../../components/LoadingModal"
@@ -39,11 +37,11 @@ const Login = () => {
 
     return(
         <div className='appPage'>
-            <div className='body-login'>  
+            <div className='body-login'> 
                 <div className='bg-login'></div>
                 <form type='submit' className='form-login' onSubmit={handleLogin}>
+                 <img className='img-login' src={salvaLucroLogo} alt='logo salva lucro' />
                     <div className='input-container'>
-                    <img className='img-login' src={salvaLucroLogo} alt='logo salva lucro' />
                         <input id='login' className='input-login' type='text' placeholder='usuário' value={login} onChange={(e) => setLogin(e.target.value)}/>
                         <input id='senha' className='input-login' type='password' placeholder='senha' value={password} onChange={(e) => setPassword(e.target.value)}/>
                         <hr/>
@@ -53,11 +51,10 @@ const Login = () => {
                     <button className='btn btn-secondary' onClick={submitFake}>Login Teste</button>
                 </form>
             </div>
-            
+
             { loading ? <LoadingModal/> : <></>}
         </div>
-
-        
+   
     )
 }
 
