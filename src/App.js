39ae8dc51edbad22428b7f35879a3449773cb8ac
Dudugs410 <dudugs_@hotmail.com>
2023-10-10 09:39:20 -0300

    ajustes para apresentação

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { ToastContainer } from 'react-toastify'
 
 function App() {
   return (
-    <BrowserRouter basename='/reactapp'>
+    <BrowserRouter>
       <AuthProvider>
       <ToastContainer
             position="top-center"
