026c412c1b078e68779b89d8c06141b1dd46cc4c
Dudugs410 <dudugs_@hotmail.com>
2023-10-25 15:57:58 -0300

    app com navegação para dashboard, vendas e recebiveis

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -50,6 +50,87 @@ const Header = () =>{
         e.preventDefault()
         await refresh()
     }
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    let tipoCliente = 1
+
+    const [arrayOpcoes, setArrayOpcoes] = useState([])
+
+    useEffect(()=>{
+        arrayOpcoes.length = 0
+        if(tipoCliente === 0){
+            setArrayOpcoes([
+                {rota: '/dashboard', nome: 'Início', id: 0},
+                {rota: '/vendas', nome: 'Vendas', id: 1},
+                {rota: '/recebiveis', nome: 'Recebíveis', id: 2},
+                {rota: '/servicos', nome: 'Serviços', id: 3},
+                {rota: '/antecipacoes', nome: 'Antecipações', id: 4},
+                {rota: '/relatorios', nome: 'Relatórios', id: 5},
+                {rota: '/teste', nome: 'TESTE', id: 6}])
+        } else if(tipoCliente === 1){
+            setArrayOpcoes([
+                {rota: '/dashboard', nome: 'Início', id: 0},
+                {rota: '/vendas', nome: 'Vendas', id: 1},
+                {rota: '/recebiveis', nome: 'Recebíveis', id: 2},
+            ])
+        }
+    },[])
+
+    useEffect(()=>{
+        console.log('opcoes disponiveis: ', arrayOpcoes)
+    },[arrayOpcoes])
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    return(
+        <>
+        <div className="header-bg-image">
+                <div className='header-bg'>
+                    <div className='navbar-title'>
+                        <img className='img-header' src={salvaLucroLogoBranco} alt='logo salva lucro'/>
+                    </div>
+                    <div className='navbar-customer-wrapper'>
+                        <div className='navbar-customer'>
+                            <span>{`${nomeCliente}`}</span>
+                            <span>{`${headerCnpj}`}</span>
+                        </div>
+                        <div className='navbar-customer'>
+                            <span className='client-name'>{`${nome}`}</span>
+                            <span className='client-code'>{`${email}`}</span> 
+                        </div>              
+                    </div>
+                    <div className='btn-container'>
+                        <button type='button' className='btn-exit' onClick={logout}><FiPower color="#000000" size={24}/></button>
+                        <div class="toggle-container">
+                            <label class="switch">
+                                <input type="checkbox" id="toggleButton" />
+                                <span class="slider"></span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div className='header-content'>
+                    <div className="li-container">
+                        <ul className="navbar-nav">
+                            { arrayOpcoes.length > 0 && arrayOpcoes.map((opcao) => {
+                                return(
+                                    <li className="nav-item" key={opcao.id}>
+                                        <Link to={opcao.rota} className="nav-hover nav-text nav-link active text-shadow" aria-current="page">
+                                            <button className="li-button-content">
+                                            <span className="li-btn-text">{opcao.nome}</span>&nbsp;&nbsp;&nbsp;<FiHome/>
+                                            </button>
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+    ////////////////////////////////////////////////////////////////////////////////////
 
     return(
         <>
