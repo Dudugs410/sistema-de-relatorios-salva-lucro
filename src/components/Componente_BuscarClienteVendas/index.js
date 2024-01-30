dd23b20dda4a04e3e0a3ad93df652b680ce2237e
Dudugs410 <dudugs_@hotmail.com>
2024-01-30 11:25:16 -0300

    fix: estabilidade nas buscas em vendas e creditos

--- src/components/Componente_BuscarClienteVendas/index.js
+++ src/components/Componente_BuscarClienteVendas/index.js
@@ -1,181 +1,182 @@
-import { useState, useEffect, useContext, useRef } from "react"
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-unused-vars */
+import { useState, useEffect, useContext, useRef } from 'react'
 
 import './buscarVendas.scss'
-import { AuthContext } from "../../contexts/auth"
-import { VendasContext } from "../../pages/Vendas"
+import { AuthContext } from '../../contexts/auth'
+import { VendasContext } from '../../pages/Vendas'
 
-import { toast } from 'react-toastify';
+import { toast } from 'react-toastify'
 import { ToastContainer } from 'react-toastify'
 
 import '../../styles/global.scss'
 import 'react-toastify/dist/ReactToastify.css'
 import './reactdatepicker.css'
-import Cookies from "js-cookie";
-import { useCallback } from "react";
+import Cookies from 'js-cookie'
+import { useCallback } from 'react'
 
 const BuscarClienteVendas = () => {
-    const [buscou, setBuscou] = useState(false)
-    const [arrayDados, setArrayDados] = useState([])
+	const [buscou, setBuscou] = useState(false)
+	const [arrayDados, setArrayDados] = useState([])
 
-    const { 
-        setCnpj,  
-        setLoading,
-        loadVendas,
-        dateConvertSearch,
-        setTotaisGlobal,
-        isDarkTheme,
-        vendas,
-        gerarDados,
-    } = useContext(AuthContext)
+	const { 
+		setCnpj,  
+		setLoading,
+		loadVendas,
+		dateConvertSearch,
+		setTotaisGlobal,
+		isDarkTheme,
+		vendas,
+		gerarDados,
+	} = useContext(AuthContext)
 
-    const {
-        detalhes,
-        setDetalhes,
-        dataBusca,
-        cnpjBusca,
-        setCnpjBusca,
-        setTotalDebito,
-        setTotalCredito,
-        setTotalVoucher,
-        setTotalLiquido,
-        setArrayAdm,
-    } = useContext(VendasContext)
+	const {
+		detalhes,
+		setDetalhes,
+		dataBusca,
+		cnpjBusca,
+		setCnpjBusca,
+		setTotalDebito,
+		setTotalCredito,
+		setTotalVoucher,
+		setTotalLiquido,
+		setArrayAdm,
+	} = useContext(VendasContext)
     
-    useEffect(()=>{
-        console.log('Detalhes: ',detalhes)
-    },[detalhes])
+	useEffect(()=>{
+		console.log('Detalhes: ',detalhes)
+	},[detalhes])
     
-    useEffect(()=>{
-        setCnpj(Cookies.get('cnpj'))
-        setCnpjBusca(Cookies.get('cnpj'))
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
+	useEffect(()=>{
+		setCnpj(Cookies.get('cnpj'))
+		setCnpjBusca(Cookies.get('cnpj'))
+	},[])
 
-    const alerta = useCallback((text) => {
-        toast.info(text, {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: true,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
-      }, [])
+	const alerta = useCallback((text) => {
+		toast.info(text, {
+			position: 'top-center',
+			autoClose: 5000,
+			hideProgressBar: true,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: 'light',
+		})
+	}, [])
 
-      async function handleBusca(e){
-        e.preventDefault()
-        if(cnpjBusca === '' || cnpjBusca === 'Selecione' || cnpjBusca === undefined){
-            alerta('selecione um cliente válido')
-            return
-        }
-        console.log('handleBusca()')
-        await buscar()
-        console.log('vendas gerar dados',vendas)
-        await gerarDados(vendas)
-}
+	async function handleBusca(e){
+		e.preventDefault()
+		if(cnpjBusca === '' || cnpjBusca === 'Selecione' || cnpjBusca === undefined){
+			alerta('selecione um cliente válido')
+			return
+		}
+		console.log('handleBusca()')
+		await buscar()
+		console.log('vendas gerar dados',vendas)
+		await gerarDados(vendas)
+	}
 
-    async function buscar() {
-        console.log('buscar()')
-        console.log(dataBusca, cnpjBusca)
-        if(cnpjBusca === '' || cnpjBusca === 'Selecione' || cnpjBusca === undefined){
-            return
-        }
-        await loadVendas(dataBusca, cnpjBusca)
-        .then(() =>{
-            if(dataBusca === '' || cnpjBusca === ''){
-                return 0
-            }
-            else{
-                alerta(`executou a busca do dia ${dateConvertSearch(dataBusca)}`)
-                setBuscou(true)
-                if(vendas.length === 0){
-                    setDetalhes(false)
-                }
-            }    
-        })
-        setLoading(false)
-    }
+	async function buscar() {
+		console.log('buscar()')
+		console.log(dataBusca, cnpjBusca)
+		if(cnpjBusca === '' || cnpjBusca === 'Selecione' || cnpjBusca === undefined){
+			return
+		}
+		await loadVendas(dataBusca, cnpjBusca)
+			.then(() =>{
+				if(dataBusca === '' || cnpjBusca === ''){
+					return 0
+				}
+				else{
+					alerta(`executou a busca do dia ${dateConvertSearch(dataBusca)}`)
+					setBuscou(true)
+					if(vendas.length === 0){
+						setDetalhes(false)
+					}
+				}    
+			})
+		setLoading(false)
+	}
 
-    useEffect(()=>{
-    console.log('buscou: ', buscou)
-    if((cnpjBusca === '' || cnpjBusca === 'Selecione' || cnpjBusca === undefined) && (Cookies.get('cnpj') !== '')){
-        alerta('selecione um cliente válido')
-        return
-    }
-    if(buscou === true){
-            if((vendas === null) || (vendas.length === 0)){
-                alerta('não existem vendas para a data selecionada')
-                setBuscou(false)
-                setDetalhes(false)
-            }
-            else{
-                setDetalhes(true)
-                setBuscou(false)
-            }
-        }
-    },[buscou])
+	useEffect(()=>{
+		console.log('buscou: ', buscou)
+		if((cnpjBusca === '' || cnpjBusca === 'Selecione' || cnpjBusca === undefined) && (Cookies.get('cnpj') !== '')){
+			alerta('selecione um cliente válido')
+			return
+		}
+		if(buscou === true){
+			if((vendas === null) || (vendas.length === 0)){
+				alerta('não existem vendas para a data selecionada')
+				setBuscou(false)
+				setDetalhes(false)
+			}
+			else{
+				setDetalhes(true)
+				setBuscou(false)
+			}
+		}
+	},[buscou])
 
-    const alertaRef = useRef()
-    const setDetalhesRef = useRef()
-    const arrayDadosRef = useRef()
+	const alertaRef = useRef()
+	const setDetalhesRef = useRef()
+	const arrayDadosRef = useRef()
 
-    useEffect(()=>{
-        alertaRef.current = alerta
-        setDetalhesRef.current = setDetalhes
-        arrayDadosRef.current = arrayDados                   
-    },[alerta, setDetalhes, arrayDados])
+	useEffect(()=>{
+		alertaRef.current = alerta
+		setDetalhesRef.current = setDetalhes
+		arrayDadosRef.current = arrayDados                   
+	},[alerta, setDetalhes, arrayDados])
 
-    useEffect(()=>{
-    console.log('buscou: ', buscou)
-    if(buscou === true){
-            if((arrayDadosRef === null) || (arrayDadosRef.length === 0)){
-                alertaRef.current('não existem vendas para a data selecionada')
-                setBuscou(false)
-            }
-            else{
-                setDetalhesRef.current(true)
-                setBuscou(false)
-            }
-        }
-    },[buscou])
+	useEffect(()=>{
+		console.log('buscou: ', buscou)
+		if(buscou === true){
+			if((arrayDadosRef === null) || (arrayDadosRef.length === 0)){
+				alertaRef.current('não existem vendas para a data selecionada')
+				setBuscou(false)
+			}
+			else{
+				setDetalhesRef.current(true)
+				setBuscou(false)
+			}
+		}
+	},[buscou])
 
-    function handleVoltar(e){
-        e.preventDefault()
-        setDetalhes(false)
-        setBuscou(false)
-        setTotalLiquido(0.00)
-        setTotalCredito(0.00)
-        setTotalDebito(0.00)
-        setTotalVoucher(0.00)
-        setTotaisGlobal({debito: 0, credito: 0, voucher: 0, liquido: 0})
-        setArrayAdm()
-    }
+	function handleVoltar(e){
+		e.preventDefault()
+		setDetalhes(false)
+		setBuscou(false)
+		setTotalLiquido(0.00)
+		setTotalCredito(0.00)
+		setTotalDebito(0.00)
+		setTotalVoucher(0.00)
+		setTotaisGlobal({debito: 0, credito: 0, voucher: 0, liquido: 0})
+		setArrayAdm()
+	}
 
-    return(
-        <>
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-        />
-            <div className='search-bar'>
-                <form className={`date-container-vendas ${isDarkTheme === true ? 'dark-theme' : 'light-theme'}`}>       
-                    <div className='submit-container select-align'>
-                        { (detalhes) && (vendas.length > 0) ? <button className={`btn btn-secondary btn-global ${isDarkTheme === true ? 'dark-theme' : 'light-theme'}`} onClick={ (e) => { handleVoltar(e) }}>Voltar</button> : <button className={`btn btn-primary btn-global ${isDarkTheme === true ? 'dark-theme' : 'light-theme'}`} onClick={handleBusca}>Pesquisar</button>}
-                    </div>      
-                </form>
-            </div>
-        </>
-    )
+	return(
+		<>
+			<ToastContainer
+				position="top-center"
+				autoClose={5000}
+				hideProgressBar
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+			/>
+			<div className='search-bar'>
+				<form className={`date-container-vendas ${isDarkTheme === true ? 'dark-theme' : 'light-theme'}`}>       
+					<div className='submit-container select-align'>
+						{ (detalhes) && (vendas.length > 0) ? <button className={`btn btn-secondary btn-global ${isDarkTheme === true ? 'dark-theme' : 'light-theme'}`} onClick={ (e) => { handleVoltar(e) }}>Voltar</button> : <button className={`btn btn-primary btn-global ${isDarkTheme === true ? 'dark-theme' : 'light-theme'}`} onClick={handleBusca}>Pesquisar</button>}
+					</div>      
+				</form>
+			</div>
+		</>
+	)
 }
 
 export default BuscarClienteVendas
--- src/components/Componente_GerarRelatorio/index.js
+++ src/components/Componente_GerarRelatorio/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from 'react'
 
 import { jsPDF } from 'jspdf'
@@ -11,100 +13,99 @@ import { imgExport } from '../../contexts/images'
 
 export default function GerarRelatorio({tableData, dataAtual}){
 
-    // EXCEL ////////////////////////////////////////////////////////////
-    const exportToExcel = () => {
+	// EXCEL ////////////////////////////////////////////////////////////
+	const exportToExcel = () => {
     
-        if (!tableData || tableData.length === 0) {
-          alert('No data to export.')
-          return
-        }
+		if (!tableData || tableData.length === 0) {
+			alert('No data to export.')
+			return
+		}
     
-        const workbook = new ExcelJS.Workbook()
-        const worksheet = workbook.addWorksheet('Sheet 1')
+		const workbook = new ExcelJS.Workbook()
+		const worksheet = workbook.addWorksheet('Sheet 1')
     
-        // Add headers
-        const headers = Object.keys(tableData[0])
-        worksheet.addRow(headers)
+		// Add headers
+		const headers = Object.keys(tableData[0])
+		worksheet.addRow(headers)
     
-        // Add data rows
-        tableData.forEach((rowData) => {
-          const values = Object.values(rowData)
-          worksheet.addRow(values);
-        })
+		// Add data rows
+		tableData.forEach((rowData) => {
+			const values = Object.values(rowData)
+			worksheet.addRow(values)
+		})
     
-        // Generate Excel file
-        workbook.xlsx.writeBuffer()
-          .then((buffer) => {
-            saveExcelFile(buffer, `Relatorio_de_vendas_${dataAtual}.xlsx`)
-          })
-          .catch((error) => {
-            console.error('Error generating Excel file:', error)
-          })
-      }
+		// Generate Excel file
+		workbook.xlsx.writeBuffer()
+			.then((buffer) => {
+				saveExcelFile(buffer, `Relatorio_de_vendas_${dataAtual}.xlsx`)
+			})
+			.catch((error) => {
+				console.error('Error generating Excel file:', error)
+			})
+	}
     
-      const saveExcelFile = (buffer, fileName) => {
-        const data = new Blob([buffer], { type: 'application/octet-stream' })
-        const url = URL.createObjectURL(data)
-        const link = document.createElement('a')
-        link.href = url
-        link.download = fileName
-        link.click()
-      }
+	const saveExcelFile = (buffer, fileName) => {
+		const data = new Blob([buffer], { type: 'application/octet-stream' })
+		const url = URL.createObjectURL(data)
+		const link = document.createElement('a')
+		link.href = url
+		link.download = fileName
+		link.click()
+	}
 
-      // PDF ////////////////////////////////////////////////////////////
+	// PDF ////////////////////////////////////////////////////////////
       
-    const generatePdf = () => {
-      if (!tableData || tableData.length === 0) {
-        alert('Sem dados para exportar')
-        return
-      }
-        let logoUrl = '../../assets/salva-lucro-logo.png';
+	const generatePdf = () => {
+		if (!tableData || tableData.length === 0) {
+			alert('Sem dados para exportar')
+			return
+		}
       
-        const columns = ['Adquirente', 'Bandeira', 'Produto', 'NSU', 'CNPJ', 'Código da Venda', 'Código da Autorização', 'Número PV', 'Valor Bruto', 'Valor Líquido', 'Taxa', 'Data da Venda', 'Hora da Venda', 'Data do Crédito', 'Qtd Parcelas'];
-        const rows = tableData.map(rowData => [rowData.adquirente, rowData.bandeira, rowData.produto, rowData.nsu, rowData.cnpj, rowData.codigoVenda, rowData.codigoAutorizacao, rowData.numeroPV, rowData.valorBruto, rowData.valorLiquido, rowData.taxa, rowData.dataVenda, rowData.horaVenda, rowData.dataCredito, rowData.parcelas]);
+		const columns = ['Adquirente', 'Bandeira', 'Produto', 'NSU', 'CNPJ', 'Código da Venda', 'Código da Autorização', 'Número PV', 'Valor Bruto', 'Valor Líquido', 'Taxa', 'Data da Venda', 'Hora da Venda', 'Data do Crédito', 'Qtd Parcelas']
+		const rows = tableData.map(rowData => [rowData.adquirente, rowData.bandeira, rowData.produto, rowData.nsu, rowData.cnpj, rowData.codigoVenda, rowData.codigoAutorizacao, rowData.numeroPV, rowData.valorBruto, rowData.valorLiquido, rowData.taxa, rowData.dataVenda, rowData.horaVenda, rowData.dataCredito, rowData.parcelas])
 
-        const doc = new jsPDF({
-        orientation: 'landscape',
-        unit: 'mm',
-        format: 'a3',
-        })
+		const doc = new jsPDF({
+			orientation: 'landscape',
+			unit: 'mm',
+			format: 'a3',
+		})
 
-        var myImg = imgExport
+		var myImg = imgExport
 
-        doc.autoTable({
-        head: [columns],
-        body: rows,
-        margin: {top: 30},
-        headStyles : {
-          fillColor : [153, 204, 51],
-          valign: 'middle',
-          halign : 'center',
-        },
-        didDrawPage: function (data) {
-          // Add an image to each page in the top right corner
-          const imageWidth = 80 // Adjust width as needed
-          const imageHeight = 13 // Adjust height as needed
-          const positionX = 310
-          const positionY = 8
+		doc.autoTable({
+			head: [columns],
+			body: rows,
+			margin: {top: 30},
+			headStyles : {
+				fillColor : [153, 204, 51],
+				valign: 'middle',
+				halign : 'center',
+			},
+			didDrawPage: function (data) {
+				// Add an image to each page in the top right corner
+				const imageWidth = 80 // Adjust width as needed
+				const imageHeight = 13 // Adjust height as needed
+				const positionX = 310
+				const positionY = 8
 
-          doc.addImage(myImg, 'JPEG', positionX, positionY, imageWidth, imageHeight);
-        }
-        })
+				doc.addImage(myImg, 'JPEG', positionX, positionY, imageWidth, imageHeight)
+			}
+		})
     
-        doc.save(`Relatorio_de_vendas_${dataAtual}.pdf`)
-    }
+		doc.save(`Relatorio_de_vendas_${dataAtual}.pdf`)
+	}
     
 
-    return(
-        <>
-            <div className='container'>
-              <div className='export-column'>
-                <button className="btn btn-exportar btn-exportar-excel" onClick={exportToExcel}>Download Excel <FiFilePlus /></button>
-              </div>
-              <div className='export-column'>
-                <button className='btn btn-exportar btn-exportar-pdf' onClick={generatePdf}>Download PDF <FiFilePlus /></button>
-              </div>
-            </div>
-        </>
-    )
+	return(
+		<>
+			<div className='container'>
+				<div className='export-column'>
+					<button className="btn btn-exportar btn-exportar-excel" onClick={exportToExcel}>Download Excel <FiFilePlus /></button>
+				</div>
+				<div className='export-column'>
+					<button className='btn btn-exportar btn-exportar-pdf' onClick={generatePdf}>Download PDF <FiFilePlus /></button>
+				</div>
+			</div>
+		</>
+	)
 }
