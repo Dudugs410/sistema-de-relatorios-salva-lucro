7920ee95418a3f3a32157a5a8ac635271ec5d881
Dudugs410 <dudugs_@hotmail.com>
2024-03-26 11:10:21 -0300

    feature: dropdown menu e tweaks nos displays de navegação

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
-import { FiMoon, FiSun, FiHome, FiDollarSign, FiCreditCard, FiRefreshCcw, FiTool, FiFileText, FiClipboard, FiDownload, FiCalendar, FiList, FiPaperclip, FiSettings, FiTruck, FiShoppingBag } from "react-icons/fi";
+import { FiMoon, FiSun, FiHome, FiDollarSign, FiCreditCard, FiRefreshCcw, FiTool, FiFileText, FiClipboard, FiDownload, FiCalendar, FiList, FiPaperclip, FiSettings, FiTruck, FiShoppingBag, FiMenu } from "react-icons/fi";
 import { AuthContext } from "../../contexts/auth";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import salvaLucroLogoBranco from '../../assets/LogoTopo.png';
 import './header.scss';
 import '../../index.scss';
@@ -106,18 +106,10 @@ const Header = () => {
         };
 
         const orderedOptions = [
-            { nome: 'Início', icone: icones['FiHome'], rota: '/dashboard', children:[
-                { nome: 'Início', rota: '/dashboard' }
-            ]},
-            { nome: 'Vendas', icone: icones['FiDollarSign'], rota: '/vendas', children:[
-                { nome: 'Vendas', rota: '/vendas' }
-            ]},
-            { nome: 'Créditos', icone: icones['FiCreditCard'], rota: '/creditos', children:[
-                { nome: 'Créditos', rota: '/creditos' }
-            ]},
-            { nome: 'Serviços', icone: icones['FiTool'], rota: '/servicos', children:[
-                { nome: 'Serviços', rota: '/servicos' }
-            ]},
+            { nome: 'Início', icone: icones['FiHome'], rota: '/dashboard'},
+            { nome: 'Vendas', icone: icones['FiDollarSign'], rota: '/vendas'},
+            { nome: 'Créditos', icone: icones['FiCreditCard'], rota: '/creditos'},
+            { nome: 'Serviços', icone: icones['FiTool'], rota: '/servicos'},
             { nome: 'Relatórios', icone: icones['FiFileText'], children: [
                 { nome: 'Financeiro', rota: '/financeiro' },
                 { nome: 'Gerenciais', rota: '/gerenciais' },
@@ -128,18 +120,10 @@ const Header = () => {
                 { nome: 'Meta', rota: '/meta' },
                 { nome: 'Meta Sapiranga', rota: '/metasapiranga' },
             ]},
-            { nome: 'Administração', icone: icones['FiPaperClip'], rota: '/administracao', children:[
-                { nome: 'Administração', rota: '/administracao' }
-            ]},
-            { nome: 'Suporte', icone: icones['FiSettings'], rota: '/suporte', children:[
-                { nome: 'Suporte', rota: '/suporte' }
-            ]},
-            { nome: 'Delivery', icone: icones['FiTruck'], children: [
-                { nome: 'Vendas Delivery', rota: '/vendasdelivery' },
-            ]},
-            { nome: 'Conciliacao', icone: icones['FiShoppingBag'], children: [
-                { nome: 'Conciliação Bancária', rota: '/conciliacao' },
-            ]},
+            { nome: 'Administração', icone: icones['FiPaperClip'], rota: '/administracao'},
+            { nome: 'Suporte', icone: icones['FiSettings'], rota: '/suporte'},
+            { nome: 'Delivery', icone: icones['FiTruck'], rota: '/vendasdelivery'},
+            { nome: 'Conciliacao', icone: icones['FiShoppingBag'], rota: '/conciliacao'},
         ];
 
         let arrayOpcoes = [];
@@ -172,6 +156,80 @@ const CustomCheckbox = ({ isChecked, handleCheckboxChange }) => {
   );
 };
 
+const SideBar = () =>{
+    useEffect(() => {
+        function handleClickOutside(event) {
+            const sidebarButton = document.querySelector('.side-bar-container .btn-primary');
+            const collapseElement = document.getElementById('multiCollapseExample1');
+            
+            // Check if click occurred outside the collapsed sidebar and the sidebar button is not clicked
+            if (collapseElement && !collapseElement.contains(event.target) && event.target !== sidebarButton) {
+                const collapse = bootstrap.Collapse.getInstance(collapseElement);
+                if (collapse && !collapse._isTransitioning) {
+                    collapse.hide();
+                }
+            }
+        }
+
+        // Add event listener to detect clicks on document body
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            // Remove event listener when component unmounts
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div className='side-bar-container'>
+            <p className='p-side-bar'>
+                <a className={`btn btn-primary btn-global a-side-bar ${isDarkTheme ? 'dark-theme' : 'light-theme'}`} data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1"><FiMenu size={30}/></a>
+            </p>
+            <div>
+                <div>
+                    <div className="collapse multi-collapse" id="multiCollapseExample1">
+                        <div>
+                            <ul className={`mobile ${isDarkTheme ? 'dark-theme' : 'light-theme' }`}>
+                                {optionsWithIcons.map((option, index) => (
+                                    <li className="li-sidebar" key={index}>
+                                        {option.children ? (
+                                            <div className="dropend">
+                                                <button className={`px-2 me-1 btn-mobile dropdown-toggle ${isDarkTheme ? 'dark-theme' : 'light-theme'}`} type="button" id={`dropdownMenuButton${index}`} data-bs-toggle="dropdown" aria-expanded="false">
+                                                    {option.icone && React.createElement(option.icone)}
+                                                    <span className="mb-auto mobile">{option.nome}</span>
+                                                </button>
+                                                <ul className="dropdown-menu" aria-labelledby={`dropdownMenuButton${index}`} style={{left: 'auto', right: 0}}>
+                                                    {option.children.map((childOption, childIndex) => (
+                                                        <li className='li-side-bar' key={childIndex}>
+                                                            <Link to={childOption.rota} className={`dropdown-item ${isDarkTheme ? 'dark-theme' : 'light-theme' }`}>
+                                                                <button className={`px-2 btn-mobile ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
+                                                                    {childOption.icone && React.createElement(childOption.icone)}
+                                                                    <span className="mb-auto mobile">{childOption.nome}</span>
+                                                                </button>
+                                                            </Link>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        ) : (
+                                            <Link to={option.rota} className="nav-hover active text-shadow">
+                                                <button className={`px-2 me-1 btn-mobile ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
+                                                    {option.icone && React.createElement(option.icone)}
+                                                    <span className="mb-auto mobile">{option.nome}</span>
+                                                </button>
+                                            </Link>
+                                        )}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+    }
+
     return (
         <>
             <div className="header-bg-image">
@@ -188,6 +246,8 @@ const CustomCheckbox = ({ isChecked, handleCheckboxChange }) => {
                         </div>
                     </div>
                 </div>
+
+                <SideBar/>
 
                 <div className='header-content'>
                     <div className={`barra-header ${isDarkTheme ? 'dark-theme' : 'light-theme' }`}>
