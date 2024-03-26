b1ed6bb90bd64b9f86c7e44721d0c1f0cead42b2
Dudugs410 <dudugs_@hotmail.com>
2024-03-26 14:28:42 -0300

    feature: botao contato

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,18 +1,29 @@
 import Header from "../Header"
 import Footer from "../Footer"
-import SeletorCliente from "../SeletorCliente"
-
-import './layout.css'
 import SeletorClienteDev from "../SeletorCliente dev"
-
+import './layout.scss'
+import { FiMail } from "react-icons/fi"
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/auth"
 
 function Layout({ children }){
+    const { isDarkTheme } = useContext(AuthContext)
+
+    const handleClick = () => {
+        console.log('handleClick')
+    }
+
     return(
         <div className='layout'>
             <div className='appPage'>
                 <Header />
                 <SeletorClienteDev/>
                 { children }
+                <div className={`btn-contato-container ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
+                    <button className={`btn-global btn-contato ${isDarkTheme ? 'dark-theme' : 'light-theme'}`} onClick={handleClick}>
+                        <span><FiMail  size={30}/></span>
+                    </button>
+                </div>
                 <Footer />
             </div>
         </div>
