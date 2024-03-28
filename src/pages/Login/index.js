108803569f247ad3ac18cfe7bed36770c9c7b172
Dudugs410 <dudugs_@hotmail.com>
2024-03-28 10:49:19 -0300

    revisao css: Login e Botão de navegação

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -53,11 +53,11 @@ const Login = () => {
                 <div className='bg-login'></div>
                 <form type='submit' className='form-login' onSubmit={handleLogin}>
                  <img className='img-login' src={salvaLucroLogo} alt='logo salva lucro' />
-                    <div className='input-container'>
+                    <div className='input-container-login'>
                         <input id='login' className='input-login' type='text' placeholder='usuário' value={login} onChange={(e) => setLogin(e.target.value)}/>
                         <input id='senha' className='input-login' type='password' placeholder='senha' value={password} onChange={(e) => setPassword(e.target.value)}/>
-                        <hr/>
-                        { !loading ? <button type='submit' className='btn btn-primary'>Login</button> : <button type='submit' className='btn btn-primary' disabled>Carregando...</button>}
+                        <hr className='hr-global' />
+                        { !loading ? <button type='submit' className='btn btn-primary'>Login</button> : <button type='submit' className='btn btn-primary' disabled>Login</button>}
                     </div>
                 </form>
             </div>
